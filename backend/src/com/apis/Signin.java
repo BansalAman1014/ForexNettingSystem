8f3c575d6e5c7687540fc35e6a77fb85b88ccc44
yaswanthraj <yaswanthraj671@gmail.com>
2018-08-24 11:39:03 +0530

    Formatted Signin.java

--- backend/src/com/apis/Signin.java
+++ backend/src/com/apis/Signin.java
@@ -20,24 +20,25 @@ import com.utils.UserUtilities;
  */
 @WebServlet("/signin")
 public class Signin extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
 	private UserUtilities userUtil = new UserUtilities();
 	private static final JsonUtility jsonUtil = new JsonUtility();
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Signin() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Signin() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setStatus(HttpServletResponse.SC_OK);
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
@@ -48,11 +49,11 @@ public class Signin extends HttpServlet {
 		try {
 			Map<String, String> responseBody = RequestResponseUtility.getRequestBody(request, jsonUtil);
 			// Begin: Validating required parameters in map
-			String[] requiredParams =  {"email", "password"};
-			List<String> userExceptions = RequestResponseUtility.validateRequiredBodyParams(responseBody, 
-					(List<String>)Arrays.asList(requiredParams));
-			if(!userExceptions.isEmpty()) {
-				String errorJson = RequestResponseUtility.logAndBuildJsonOfUserException(this.getClass().getName(), 
+			String[] requiredParams = { "email", "password" };
+			List<String> userExceptions = RequestResponseUtility.validateRequiredBodyParams(responseBody,
+					(List<String>) Arrays.asList(requiredParams));
+			if (!userExceptions.isEmpty()) {
+				String errorJson = RequestResponseUtility.logAndBuildJsonOfUserException(this.getClass().getName(),
 						"doPost", userExceptions);
 				RequestResponseUtility.buildErrorResponse(request, response, errorJson);
 				return;
@@ -61,19 +62,21 @@ public class Signin extends HttpServlet {
 			String email = responseBody.get("email");
 			String password = responseBody.get("password");
 			// Validating user with email and password
-			if(userUtil.validateUser(email, password)) {
+			if (userUtil.validateUser(email, password)) {
 				// If user validated successfully user will be sent with the token as response
 				String responseJson = jsonUtil.convertToJson("user", userUtil.getUserByEmail(email));
 				RequestResponseUtility.buildSuccessfulResponse(request, response, responseJson);
 			} else {
-				// else error response will be sent which will state user didn't validated with given credentials
+				// else error response will be sent which will state user didn't validated with
+				// given credentials
 				userExceptions.add("User doesn't exist with given credentials. Please give valid credentials.");
-				String errorJson = jsonUtil.convertToJson("errors",  userExceptions);
+				String errorJson = jsonUtil.convertToJson("errors", userExceptions);
 				RequestResponseUtility.buildErrorResponse(request, response, errorJson);
 			}
 		} catch (Exception e) {
 			// If any run time exception occurs error response will be sent
-			String errorJson = RequestResponseUtility.logAndBuildJsonOfSystemException(this.getClass().getName(), "doPost", e);
+			String errorJson = RequestResponseUtility.logAndBuildJsonOfSystemException(this.getClass().getName(),
+					"doPost", e);
 			RequestResponseUtility.buildErrorResponse(request, response, errorJson);
 		}
 	}
