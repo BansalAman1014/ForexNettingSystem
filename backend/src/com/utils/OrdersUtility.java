bbeb30937f71d853993165796981326bab91d635
amanbansal <ambansal1014@gmail.com>
2018-08-29 11:54:28 +0530

    Modified order utility

--- backend/src/com/utils/OrdersUtility.java
+++ backend/src/com/utils/OrdersUtility.java
@@ -1,6 +1,7 @@
 package com.utils;
 
 import java.sql.Date;
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -9,6 +10,7 @@ import org.hibernate.cfg.Configuration;
 
 import com.models.CurrencyPair;
 import com.models.Order;
+import com.models.OrderMapping;
 import com.models.OrderStatus;
 import com.models.OrderType;
 import com.models.User;
@@ -17,6 +19,7 @@ public class OrdersUtility {
 	static private SessionFactory sessionFactory;
 	private UserUtilities userUtil;
 	private CurrencyPairUtility currencyPairUtility;
+	private OrderMappingUtility orderMappingUtility;
 
 	public OrdersUtility() {
 		try {
@@ -25,6 +28,7 @@ public class OrdersUtility {
 			sessionFactory = config.buildSessionFactory();
 			userUtil = new UserUtilities();
 			currencyPairUtility = new CurrencyPairUtility();
+			orderMappingUtility = new OrderMappingUtility();
 		} catch (Exception e) {
 			System.out.println("Exception occurred at " + this.getClass().getName() + " and the error was " + e);
 		}
@@ -51,7 +55,6 @@ public class OrdersUtility {
 			transaction.commit();
 			return order;
 		} catch (Exception e) {
-			e.printStackTrace();
 			System.out.println("Exception ocured while saving user and exception was " + e);
 		} finally {
 			if (session != null)
@@ -60,4 +63,36 @@ public class OrdersUtility {
 		return null;
 	}
 
+	@SuppressWarnings("unchecked")
+	public OrderMapping executeOrCancelBuyOrder(Order buyOrder) {
+		Session session = null;
+		OrderMapping orderMapping = null;
+		try {
+			session = sessionFactory.openSession();
+			Transaction transaction = session.getTransaction();
+			transaction.begin();
+			String query = String.format(
+					"from Order where currencyPair_id=%d and type='SELL' and notional_amount=%f and status='OPENED' LIMIT 1",
+					buyOrder.getCurrencyPair().getId(), buyOrder.getNotionalAmount());
+			List<Order> sellOrders = (List<Order>) session.createQuery(query).list();
+			if (sellOrders.isEmpty()) {
+				buyOrder.setStatus(OrderStatus.CANCELED);
+				session.save(buyOrder);
+			} else {
+				Order sellOrder = sellOrders.get(0);
+				orderMapping = orderMappingUtility.addTransaction(buyOrder, sellOrder);
+				buyOrder.setStatus(OrderStatus.EXECUTED);
+				sellOrder.setStatus(OrderStatus.EXECUTED);
+				session.save(buyOrder);
+				session.save(sellOrder);
+			}
+			transaction.commit();
+		} catch (Exception e) {
+			System.out.println("Exception ocured while saving user and exception was " + e);
+		} finally {
+			if (session != null)
+				session.close();
+		}
+		return orderMapping;
+	}
 }
