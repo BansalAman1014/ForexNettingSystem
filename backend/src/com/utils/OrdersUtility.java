12bbe52c2ad3603659bfec2902d0d9d61c6a5759
yaswanthraj <yaswanthraj671@gmail.com>
2018-08-29 17:37:15 +0530

    Added Blotter API which will return both trade and order

--- backend/src/com/utils/OrdersUtility.java
+++ backend/src/com/utils/OrdersUtility.java
@@ -3,10 +3,13 @@ package com.utils;
 import java.sql.Date;
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
 
 import com.models.CurrencyPair;
 import com.models.Order;
@@ -73,7 +76,8 @@ public class OrdersUtility {
 			transaction.begin();
 			String query = String.format(
 					"from Order where currencyPair=%d and type='%s' and notionalAmount=%f and status='%s'",
-					buyOrder.getCurrencyPair().getId(), OrderType.SELL, buyOrder.getNotionalAmount(), OrderStatus.OPENED);
+					buyOrder.getCurrencyPair().getId(), OrderType.SELL, buyOrder.getNotionalAmount(),
+					OrderStatus.OPENED);
 			List<Order> sellOrders = (List<Order>) session.createQuery(query).list();
 			if (sellOrders.isEmpty()) {
 				buyOrder.setStatus(OrderStatus.CANCELED);
@@ -94,5 +98,39 @@ public class OrdersUtility {
 				session.close();
 		}
 		return orderMapping;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Order> getOrders(User user, List<OrderStatus> orderStatuses, String dateInMilliSec,
+			OrderType orderType) {
+		Session session = null;
+		List<Order> orders = null;
+		try {
+			session = sessionFactory.openSession();
+			Transaction transaction = session.getTransaction();
+			transaction.begin();
+			Criteria criteria = session.createCriteria(Order.class);
+			Criterion userCriterion = Restrictions.and(Restrictions.eq("user", user));
+			Criterion statusCriterion = Restrictions.in("status", orderStatuses);
+			criteria.add(userCriterion);
+			criteria.add(statusCriterion);
+			if (dateInMilliSec != null && !dateInMilliSec.equals("")) {
+				Date date = new Date(Long.parseLong(dateInMilliSec));
+				Criterion dateCriterion = Restrictions.and(Restrictions.eq("orderedDate", date));
+				criteria.add(dateCriterion);
+			}
+			if (orderType != null) {
+				Criterion typeCriterion = Restrictions.and(Restrictions.eq("type", orderType));
+				criteria.add(typeCriterion);
+			}
+			orders = (List<Order>) criteria.list();
+			transaction.commit();
+		} catch (Exception e) {
+			System.out.println("Exception ocured while saving user and exception was " + e);
+		} finally {
+			if (session != null)
+				session.close();
+		}
+		return orders;
 	}
 }
